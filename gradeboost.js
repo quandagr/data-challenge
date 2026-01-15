cfce4e486f5ade6155e77d1b53c9ff470b1c727c
Quanda Grant <quandagrant@gmail.com>
2026-01-14 22:02:09 -0500

    complete part 2

---
+++ gradeboost.js
@@ -0,0 +1,17 @@
+const students = [
+  { id: 1, name: "Alice", age: 22, courses: ["HTML", "CSS"], active: true, grade: 85 },
+  { id: 2, name: "Bob", age: 28, courses: ["JS", "React"], active: false, grade: 92 },
+  { id: 3, name: "Charlie", age: 24, courses: ["HTML", "JS"], active: true, grade: 78 },
+  { id: 4, name: "Diana", age: 30, courses: ["Node", "Supabase"], active: true, grade: 95 },
+  { id: 5, name: "Ethan", age: 19, courses: ["CSS", "Bootstrap"], active: false, grade: 60 },
+  { id: 6, name: "Fiona", age: 26, courses: ["JS", "Git"], active: true, grade: 88 }
+];
+// 1. Get names of all active students
+let gradeBoosted = students
+    .map(student => {
+        if (student.grade < 100) {
+            return { ...student, grade: student.grade + 5 > 100 ? 100 : student.grade + 5 };
+        }
+        return student;
+    });
+console.log(gradeBoosted);
